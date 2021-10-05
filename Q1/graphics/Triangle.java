a6933d26e2287e89ca42d341c72ab8039e785157
lise1905 <77263939+lise1905@users.noreply.github.com>
2021-10-05 16:48:16 +0530

    Add files via upload

---
+++ Q1/graphics/Triangle.java
@@ -0,0 +1,10 @@
+package graphics;
+
+public class Triangle implements Graphics{
+
+    @Override
+    public float area(float x, float y){
+        return ((x *y)/2);
+    }
+}
+© 2021 GitHub, Inc.
